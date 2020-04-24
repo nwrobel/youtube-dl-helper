69b8d2b50827e74521fd30fbde6f023bf02be2b8
Nick Wrobel <nickwrobel2@gmail.com>
2020-04-24 02:41:01 -0400

    improved playlist link parser and added feature to catch failed download links and report them

--- Download-AllYoutubeLinksInHTMLBlob.ps1
+++ Download-AllYoutubeLinksInHTMLBlob.ps1
@@ -6,14 +6,38 @@ function Download-AllYoutubeLinksInHTMLBlob {
     $ffmpegDir = Join-Path $PSScriptRoot -ChildPath 'dependencies\ffmpeg\bin'
     $youtubeDlFilepath = Join-Path $PSScriptRoot -ChildPath 'dependencies\youtube-dl.exe'
 
-    $pythonLinkParserScriptFilepath = Join-Path $PSScriptRoot -ChildPath 'YoutubeLinkParser.py'
-    $linksOutputFilepath = Join-Path $PSScriptRoot -ChildPath 'YoutubeLinkParser-links-output.txt'
+    $pythonLinkParserScriptFilepath = Join-Path $PSScriptRoot -ChildPath 'YoutubeLinkParser2.py'
+    $linksOutputFilepath = Join-Path $env:TEMP -ChildPath 'links-out.txt'
 
     & python.exe $pythonLinkParserScriptFilepath $HTMLFilepath $linksOutputFilepath
  
+    $youtubeDlArgs = @(
+        '-f',
+        'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio',
+        '--merge-output-format',
+        'mp4',
+        '--output',
+        '"%(uploader)s - %(title)s - YouTube [%(id)s]"', 
+        '--ffmpeg-location',
+        "$ffmpegDir"
+    )
+
     $links = Get-Content $linksOutputFilepath
+    $failedLinks = @()
     foreach ($videoUrl in $links) {
-        & "$youtubeDlFilepath" -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4 --output "%(uploader)s - %(title)s - YouTube [%(id)s]" --ffmpeg-location "$ffmpegDir" "$videoUrl"
-    }  
+        $thisVideoDownloadArgs = $youtubeDlArgs + "$videoUrl"
+
+        $process = Start-Process -FilePath $youtubeDlFilepath -ArgumentList $thisVideoDownloadArgs -PassThru -Wait -NoNewWindow
+        #& "$youtubeDlFilepath" -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4 --output "%(uploader)s - %(title)s - YouTube [%(id)s]" --ffmpeg-location "$ffmpegDir" "$videoUrl"
+        
+        if ($process.ExitCode -ne 0) {
+            $failedLinks += $videoUrl
+        }
+    }
+    
+    Write-Host "The following links failed to download:`n$($failedLinks)"
 }
+
+$htmlFilepath = "Z:\Development\Test Data\youtube-utils\src.txt"
+Download-AllYoutubeLinksInHTMLBlob -HTMLFilepath $htmlFilepath
 
--- YoutubeLinkParser2.py
+++ YoutubeLinkParser2.py
@@ -1,10 +1,31 @@
+import argparse
 from bs4 import BeautifulSoup
 
-htmlFilepath = "D:\\VM-Shared\\silicon\\src.txt"
-soup = BeautifulSoup (open(htmlFilepath, encoding='utf-8'), 'html.parser')
 
-#links = soup.find_all('ytd-playlist-video-renderer')
-links = soup.find_all('a', {'class':['yt-simple-endpoint style-scope', 'ytd-playlist-video-renderer']})
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
 
-for link in links:
-    print(link)
+    parser.add_argument("inputFilepath", type=str, help=\
+        '''The absolute filepath of the input file containing the HTML blob data to parse and find links in.'''
+    )
+
+    parser.add_argument("outputFilepath", type=str, help=\
+        '''The absolute filepath of the where the output file containing the found Youtube video links should be saved.'''
+    )
+      
+    args = parser.parse_args()
+
+    soup = BeautifulSoup (open(args.inputFilepath, encoding='utf-8'), 'html.parser')
+
+    playlistItemContainers = soup.find_all('a', {'class':['yt-simple-endpoint style-scope', 'ytd-playlist-video-renderer']})
+
+    playlistVideoLinks = []
+    for container in playlistItemContainers:
+        link = container.attrs['href']
+        videoId = link.split('&')[0]
+        videoLink = "youtube.com{}".format(videoId)
+        playlistVideoLinks.append(videoLink)
+
+    with open(args.outputFilepath, mode='w', encoding='utf-8') as f:
+        for link in playlistVideoLinks:
+            f.write("{}\n".format(link))
